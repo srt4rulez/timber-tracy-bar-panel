53c980e6ad2e565c2ea3da09c289b349d6cd2844
Jake Dennison <srt4rulez@gmail.com>
2017-02-22 18:48:34 -0500

    Add TimberBarPanel file

---
+++ src/TimberBarPanel.php
@@ -0,0 +1,119 @@
+<?php
+namespace srt4rulez;
+
+use Tracy\Dumper;
+use Timber\Timber;
+
+class TimberBarPanel implements \Tracy\IBarPanel
+{
+    protected $renderData      = [];
+    protected $renderFiles     = [];
+    protected $callingPhpFiles = [];
+
+    /**
+     * Use `add_filter` functions to get some of timbers data
+     */
+    public function __construct()
+    {
+        if (!$this->doesTimberExist()) {
+            return;
+        }
+
+        add_filter('timber/loader/render_data', [$this, 'setRenderData']);
+        add_filter('timber_render_file', [$this, 'addRenderFile']);
+        // add_filter('timber/calling_php_file', [$this, 'addCallingPhpFile']);
+    }
+
+    /**
+     * Check if Timber class exists
+     *
+     * @return bool
+     */
+    protected function doesTimberExist()
+    {
+        return class_exists('Timber');
+    }
+
+    /**
+     * Set $renderData from `timber/loader/render_data` filter
+     *
+     * @param array $data
+     * @return array
+     */
+    public function setRenderData($data)
+    {
+        $this->renderData = $data;
+        return $data;
+    }
+
+    /**
+     * Add each rendered file to our $renderFiles array from the `timber_render_file`
+     * filter
+     *
+     * @param string $file
+     * @return string
+     */
+    public function addRenderFile($file)
+    {
+        $this->renderFiles[] = $file;
+        return $file;
+    }
+
+    /**
+     * Add each called php file to our $callingPhpFiles array from the `timber/calling_php_file`
+     * filter
+     *
+     * @TODO: Add this to info and make it look good
+     * @param string $file
+     * @return string
+     */
+    public function addCallingPhpFile($file)
+    {
+        $this->callingPhpFiles[] = $file;
+        return $file;
+    }
+
+    /**
+     * Return the HTML for the tab portion of this bar panel
+     *
+     * @return string
+     */
+	public function getTab()
+	{
+        ob_start();
+
+		require __DIR__ . '/../assets/tab.html';
+
+		return ob_get_clean();
+	}
+
+    /**
+     * Return the HTML for the panel portion of this bar panel
+     *
+     * @return string
+     */
+	public function getPanel()
+	{
+        if (!$this->doesTimberExist()) {
+            return '<h1>Timber class is not loaded/activated.</h1>';
+        }
+
+        ob_start();
+
+        $info = [
+            'Rendered Twig File(s)' => $this->renderFiles,
+            'Version'               => Timber::$version,
+            'Template Dirs'         => Timber::$dirname,
+            'Twig Cache'            => Helpers::getHumanReadableBool(Timber::$twig_cache),
+            'Cache'                 => Helpers::getHumanReadableBool(Timber::$cache),
+            'Auto Meta'             => Helpers::getHumanReadableBool(Timber::$auto_meta),
+            'Autoescape'            => Helpers::getHumanReadableBool(Timber::$autoescape),
+        ];
+
+        $context = Dumper::toHtml($this->renderData);
+
+        require __DIR__ . '/../assets/panel.php';
+
+		return ob_get_clean();
+	}
+}
